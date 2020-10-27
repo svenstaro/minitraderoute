297222d59f52c822959e4c3bb20948df30d1860f
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-10-27 10:59:19 +0100

    Remove bevy and use other libs

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,86 @@
-use bevy::app::AppExit;
-use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
+use anyhow::{Context, Result};
 
-enum ResourceType {
-    Minerals,
-}
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::{LogicalSize, PhysicalSize},
+    event::{Event, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
 
-struct Planet;
-struct Ship;
-struct Station;
+use rand::Rng;
+use rand_xoshiro::rand_core::SeedableRng;
+use rand_xoshiro::Xoroshiro128StarStar;
 
-fn main() {
-    App::build()
-        .add_default_plugins()
-        .add_startup_system(setup.system())
-        .add_system(game_exit_system.system())
-        .add_system(ship_movement_system.system())
-        .run();
-}
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 800;
+const GAME_WIDTH: u32 = 100;
+const GAME_HEIGTH: u32 = 100;
 
-fn game_exit_system(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut app_exit_events: ResMut<Events<AppExit>>,
-) {
-    if keyboard_input.pressed(KeyCode::Escape) {
-        app_exit_events.send(AppExit);
-    }
-}
+fn main() -> Result<()> {
+    let mut rng = Xoroshiro128StarStar::seed_from_u64(1337);
 
-fn ship_movement_system(time: Res<Time>, mut ship_query: Query<(&Ship, &mut Transform)>) {
-    let direction = Vec3::new(0.5, 0.5, 0.0).normalize();
-    for (ship, mut transform) in &mut ship_query.iter() {
-        transform.translate(100.0 * direction * time.delta_seconds);
-    }
-}
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Raqote")
+            .with_inner_size(size)
+            .with_resizable(false)
+            .build(&event_loop)
+            .unwrap()
+    };
 
-fn setup(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-    let red = materials.add(Color::rgb(0.8, 0.2, 0.2).into());
-    let blue = materials.add(Color::rgb(0.2, 0.2, 0.8).into());
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(GAME_WIDTH, GAME_HEIGTH, surface_texture)?
+    };
 
-    commands
-        .spawn(Camera2dComponents::default())
-        .spawn(UiCameraComponents::default())
-        .spawn(primitive(
-            blue,
-            &mut meshes,
-            ShapeType::RoundedRectangle {
-                width: 100.0,
-                height: 100.0,
-                border_radius: 20.0,
-            },
-            TessellationMode::Fill(&FillOptions::default()),
-            Vec3::new(200.0, 0.0, 0.0).into(),
-        ))
-        .with(Ship);
+    let mut resize_count = 0;
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            if resize_count > 0 {
+                let size = window.inner_size();
+                pixels.resize(size.width, size.height);
+                resize_count -= 1;
+                dbg!(resize_count);
+            }
+
+            let frame = pixels.get_frame();
+            for pixel in frame.chunks_exact_mut(4) {
+                pixel[0] = rng.gen();
+                pixel[1] = rng.gen();
+                pixel[2] = rng.gen();
+                pixel[3] = 0xff;
+            }
+            if pixels
+                .render()
+                .map_err(|e| eprintln!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        if input.update(&event) {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            if let Some(size) = input.window_resized() {
+                // Resize many times to work around this bug: https://github.com/parasyte/pixels/issues/121
+                // Yes, it's stupid, but it works.
+                resize_count = 100;
+                pixels.resize(size.width, size.height);
+            }
+
+            window.request_redraw();
+        }
+    });
 }
