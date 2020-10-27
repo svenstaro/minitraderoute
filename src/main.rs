f0cfcf09caa3361ab8d86afefe8aff0408511f31
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-10-27 01:20:33 +0100

    Draw multiple shapes maybe

--- src/main.rs
+++ src/main.rs
@@ -29,18 +29,35 @@ fn game_exit_system(
     }
 }
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>, mut meshes: ResMut<Assets<Mesh>>) {
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
     let red = materials.add(Color::rgb(0.8, 0.2, 0.2).into());
+    let blue = materials.add(Color::rgb(0.2, 0.2, 0.8).into());
 
     commands
         .spawn(Camera2dComponents::default())
         .spawn(UiCameraComponents::default())
         .spawn(primitive(
+            blue,
+            &mut meshes,
+            ShapeType::Circle(100.0),
+            TessellationMode::Fill(&FillOptions::default()),
+            Vec3::new(200.0, 0.0, 0.0).into(),
+        ))
+        .with(Planet)
+        .with(Position(Vec2::new(50.0, 50.0)))
+        .spawn((
+            Planet,
+            Position(Vec2::new(50.0, 50.0)),
+            primitive(
                 red,
                 &mut meshes,
-                ShapeType::Circle(60.0),
+                ShapeType::Circle(10.0),
                 TessellationMode::Fill(&FillOptions::default()),
-                Vec3::new(0.0, 0.0, 0.0).into(),
-        ))
-        .spawn((Planet, Position(Vec2::new(50.0, 50.0))));
+                Vec3::new(2.0, 0.0, 0.0).into(),
+            ),
+        ));
 }
