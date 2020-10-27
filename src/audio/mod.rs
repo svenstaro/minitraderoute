8b872544fb826cee87e5af2a85b1651d80a0c44d
Rafael Epplee <raffomania@gmail.com>
2020-10-27 16:50:46 +0100

    add more music, more samples

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,4 +1,4 @@
-use std::{time::Duration, fs::File, sync::{Arc, mpsc::Receiver}, thread, time::Instant};
+use std::{fs::File, cmp::min, sync::{Arc, mpsc::Receiver}, thread, time::Duration, time::Instant};
 use std::io::BufReader;
 use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink, Source};
 
@@ -8,42 +8,88 @@ use sound::Sound;
 // 1m nanos per 1k ms per 60 minutes
 const NANOS_PER_MINUTE: u64 = 1_000_000 * 1000 * 60;
 
-// 124 bpm
-const NANOS_PER_BEAT: u64 = NANOS_PER_MINUTE / 124;
+const NANOS_PER_BEAT: u64 = NANOS_PER_MINUTE / 120;
+
+const NANOS_PER_TICK: u64 = NANOS_PER_BEAT / 16;
 
 pub enum AudioEvent {
-    Bass
+    Bass,
+    Kick
+}
+
+struct Pattern {
+    note_slots: Vec<u8>
 }
 
 pub fn start(recv: Receiver<AudioEvent>) {
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
     let start_time = Instant::now();
 
     let bass_jab = 
-        sound::Sound::load(&include_bytes!("../../assets/bass_jab.wav")[..]);
+        Sound::load(&include_bytes!("../../assets/bass_jab.wav")[..]);
+    let kick = 
+        Sound::load(&include_bytes!("../../assets/kick_1.wav")[..]);
+    let blip = 
+        Sound::load(&include_bytes!("../../assets/blip.wav")[..]);
+
+    let mut current_note = 0;
+    let mut current_beat = 0;
+    let mut current_bar = 0;
 
     loop {
         wait_for_tick(start_time);
         let msg = recv.try_recv();
         match msg {
             Ok(AudioEvent::Bass) => {
-                sink.append(bass_jab.decoder())
+                stream_handle
+                    .play_raw(bass_jab.decoder().convert_samples())
+                    .unwrap();
             },
             _ => {}
         }
 
-        sink.append(bass_jab.decoder());
+        if current_note == 0 {
+            stream_handle
+                .play_raw(kick.decoder().convert_samples())
+                .unwrap();
+        }
+
+        if current_note == 8 {
+            stream_handle
+                .play_raw(bass_jab.decoder().convert_samples())
+                .unwrap()
+        }
+
+        if current_beat == 3 && current_note == 8 {
+            let source = blip
+                .decoder()
+                .buffered()
+                .reverb(Duration::from_nanos(NANOS_PER_TICK * 2), 0.1);
+
+            stream_handle
+                .play_raw(source.convert_samples())
+                .unwrap();
+        }
+
+        current_note = (current_note + 1) % 16;
+        if current_note == 0 {
+            current_beat = (current_beat + 1) % 4;
+        }
+        if current_note == 0 && current_beat == 0 {
+            current_bar = (current_bar + 1) % 4; 
+        }
+        println!("{} note", current_note);
     }
 }
 
 fn wait_for_tick(start_time: Instant) {
     let since_start: u64 = Instant::now().duration_since(start_time).as_nanos() as u64;
-    let since_last_beat: u64 = since_start % NANOS_PER_BEAT;
-    let until_next_beat = NANOS_PER_BEAT - since_last_beat;
-    println!("{:>8} since start", since_start / 1000);
-    println!("{:>8} since last tick", since_last_beat / 1000);
-    println!("{:>8} until next tick", until_next_beat / 1000);
+    let since_last_tick: u64 = since_start % NANOS_PER_TICK;
+    let until_next_tick = NANOS_PER_TICK - since_last_tick;
+
+    println!("{:>8} since last tick", since_last_tick / 1000);
+    println!("{:>8} until next tick", until_next_tick / 1000);
     println!("");
-    thread::sleep(Duration::from_nanos(until_next_beat));
+
+    thread::sleep(Duration::from_nanos(until_next_tick));
 }
